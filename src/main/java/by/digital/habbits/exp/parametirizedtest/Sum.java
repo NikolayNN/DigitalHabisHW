e994f8d77ce0d117e950a04a7288a0c8c9608e16
Nikolay <nikolay.horushko@gmail.com>
2023-03-04 14:34:37 +0300

    parametirized test

---
+++ src/main/java/by/digital/habbits/exp/parametirizedtest/Sum.java
@@ -0,0 +1,7 @@
+package by.digital.habbits.exp.parametirizedtest;
+
+public class Sum {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/by/digital/habbits/exp/parametirizedtest/JUnit5DataProviderExample.java
@@ -0,0 +1,28 @@
+package by.digital.habbits.exp.parametirizedtest;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class JUnit5DataProviderExample {
+
+    private final Sum myObject = new Sum();
+
+    @ParameterizedTest
+    @MethodSource("method1DataProvider")
+    public void method1(int expected, int param1, int param2) {
+        assertEquals(expected, myObject.sum(param1, param2));
+    }
+
+    static Stream<Arguments> method1DataProvider() {
+        return Stream.of(
+                arguments(10, 7, 3),
+                arguments(15, 10, 5)
+        );
+    }
+}
