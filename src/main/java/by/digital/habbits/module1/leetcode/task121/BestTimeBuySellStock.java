ea886617463bfcb2e117156d1c209c85e288a859
Nikolay <nikolay.horushko@gmail.com>
2023-03-09 21:55:03 +0300

    task 121, refactoring

--- src/main/java/by/digital/habbits/module1/leetcode/task121/BestTimeBuySellStock.java
+++ src/main/java/by/digital/habbits/module1/leetcode/task121/BestTimeBuySellStock.java
@@ -3,9 +3,6 @@ package by.digital.habbits.module1.leetcode.task121;
 public class BestTimeBuySellStock {
     static class Solution {
         public int maxProfit(int[] prices) {
-            if (prices == null || prices.length <= 1) {
-                return 0;
-            }
             int buy = prices[0];
             int profit = 0;
             for (int i = 1; i < prices.length; i++) {
